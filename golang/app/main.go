09e3c6318d5fcb7f0d3074adfaeb4f391b3ca938
IkezoeMakoto <150283t@gmail.com>
2018-09-22 22:58:08 +0900

    go cli

--- golang/app/main.go
+++ golang/app/main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/IkezoeMakoto/etc-trial/golang/app/service"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	// todo: csv から取得
-	urls := []string{"http://ua-detector.zoe.tools", "https://coin-step.com"}
+	app := cli.NewApp()
+	app.Name = "go-encryptor"
+	app.Usage = "A tool that can freely encrypt or decrypt text! It is easier than making instant noodles."
 
-	s := service.NewCheckStatusService(urls)
-	statuses := s.GetAllStatus()
+	os.Setenv("SAMPLE_ENV", "sample env dayo")
+
+	// flags
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "mode, m",
+			Value: "encrypt",
+			Usage: "encrypt or decrypt",
+		},
+	}
+
+	// action
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("-- Action --")
+
+		fmt.Printf("c.GlobalFlagNames() : %+v\n", c.GlobalFlagNames())
+		fmt.Printf("c.String(\"lang\")    : %+v\n", c.String("lang"))
+		fmt.Printf("c.String(\"m\")       : %+v\n", c.String("m"))
+		fmt.Printf("c.String(\"time\")    : %+v\n", c.String("time"))
+		fmt.Printf("c.String(\"a\")       : %+v\n", c.String("a"))
+
+		return nil
+	}
+
+	app.Run(os.Args)
 }

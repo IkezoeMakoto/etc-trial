9563d55ffceb39b10d18ce2ee44fa44fe147fb66
IkezoeMakoto <150283t@gmail.com>
2023-03-12 19:56:37 +0900

    feat: 暗号化して保存するカラムsecret_numberを追加

--- laravel10/test_app/app/Models/User.php
+++ laravel10/test_app/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'secret_number'
     ];
 
     /**
@@ -31,6 +33,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'secret_number'
     ];
 
     /**
@@ -41,4 +44,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return Attribute
+     */
+    protected function secretNumber(): Attribute
+    {
+        // NOTE: 暗号化・複合する
+        return Attribute::make(
+            get: fn (string $text) => decrypt($text),
+            set: fn (int $number) => encrypt($number)
+        );
+    }
 }
